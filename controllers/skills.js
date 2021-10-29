61d8d18f133b7b627a5a5cb3096eab2a37a200e8
jg932 <jared.h.goldstein@gmail.com>
2021-10-29 16:10:15 -0400

    part 2

--- controllers/skills.js
+++ controllers/skills.js
@@ -13,12 +13,33 @@ function index(req, res) {
   skillsDb.find({}, function(error, skills) {
     res.render('skills/index', {
       skills: skills,
-      error: error
+      error: error,
+      time: req.time,
     })
+  })
+}
+
+function newSkill(req, res) {
+  res.render("skills/new")
+}
+
+function create(req, res) {
+  console.log(req.body)
+  skillsDb.create(req.body, function(error, skill) {
+    res.redirect('/skills')
+  })
+}
+
+function deleteSkill(req, res) {
+  skillsDb.findByIdAndDelete(req.params.id, function(error, skill) {
+    res.redirect('/skills')
   })
 }
 
 export {
   show,
-  index
+  index,
+  newSkill as new,
+  create,
+  deleteSkill as delete,
 }
--- routes/skills.js
+++ routes/skills.js
@@ -8,7 +8,10 @@ const router = Router()
 // })
 
 router.get('/', skillsCtrl.index)
+router.get('/new', skillsCtrl.new)
 router.get('/:id', skillsCtrl.show)
+router.post('/', skillsCtrl.create)
+router.delete('/:id', skillsCtrl.delete)
 
 export {
   router
